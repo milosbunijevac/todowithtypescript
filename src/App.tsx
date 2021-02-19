aad40b81e52a55c712491d0161ff39c1c8631eaa
Milos Bunijevac <milos.bunijevac@gmail.com>
2021-02-18 20:40:21 -0500

    Initial App.tsx setup with working functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import './App.css';
 
-const test = 'test';
-
-function App() {
-    return <div className="App">Hello there.</div>;
+interface AppProps {
+    color?: string;
 }
 
+interface AppState {
+    counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+    // We need to pass in AppState and define an interface for it if we use the constructor
+    constructor(props: AppProps) {
+        super(props);
+        this.state = { counter: 0 };
+    }
+
+    onIncrement = (): void => {
+        this.setState({ counter: this.state.counter + 1 });
+    };
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1 });
+    };
+
+    render(): JSX.Element {
+        return (
+            <div>
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.onDecrement}>Decrement</button>
+                {this.state.counter}
+            </div>
+        );
+    }
+}
 export default App;
