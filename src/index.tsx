7c17465bacadc868dbc96e20be68e0b63b637a1d
Milos Bunijevac <milos.bunijevac@gmail.com>
2021-02-18 20:47:18 -0500

    Basic redux setup done

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reducers } from './reducers/index';
 import './index.css';
-import App from './App';
+import App from './components/App';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
     document.getElementById('root'),
 );
